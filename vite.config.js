367f070fd89c5444f355ef18d60950fe46f1b717
minorcell <minorcell6789@gmail.com>
2024-11-14 23:26:09 +0800

    pnpm: 安装了GZIP

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,18 @@ import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import gzipPlugin from 'rollup-plugin-gzip';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), AutoImport({
-    resolvers: [ElementPlusResolver()],
-  }),
-  Components({
-    resolvers: [ElementPlusResolver()],
-  }),],
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+    gzipPlugin()
+  ],
 })
