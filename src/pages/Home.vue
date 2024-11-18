39b8f13f4bc11023801f66e2196c45cff63182bf
minorcell <minorcell6789@gmail.com>
2024-11-18 19:19:04 +0800

    feat: 完成开发

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,73 +1,40 @@
 <script setup>
-import { ref } from 'vue';
-import { use } from 'echarts/core';
-import { CanvasRenderer } from 'echarts/renderers';
-import { PieChart } from 'echarts/charts';
-import { TooltipComponent, LegendComponent } from 'echarts/components';
-import VueECharts from 'vue-echarts';
+import { ref, onMounted, watch } from 'vue';
 import { getDashBoardServer } from '../api/home';
+import { useRouter } from 'vue-router'
+import * as echarts from "echarts";
 
-use([CanvasRenderer, PieChart, TooltipComponent, LegendComponent]);
-
-defineExpose({ VueECharts });
 
 const recentBlog = ref([]);
 const recentWork = ref([]);
 const recentInfo = ref([]);
+const modulesCounts = ref()
+const blogTypes = ref([])
 
+const router = useRouter();
 
 const getDashBoardData = async () => {
     const res = await getDashBoardServer();
+    const { recent_blog, recent_info, recent_work, counts, blog_type } = res.data.data;
 
-    recentBlog.value = res.data.data.recent_blog;
-    recentWork.value = res.data.data.recent_work;
-    recentInfo.value = res.data.data.recent_info;
+    recentBlog.value = recent_blog;
+    recentWork.value = recent_work;
+    recentInfo.value = recent_info;
+    modulesCounts.value = counts;
+    blogTypes.value = blog_type;
+
 }
 getDashBoardData();
-
-
-const blogTypeChartOptions = ref({
-    tooltip: {
-        trigger: 'item',
-    },
-    legend: {
-        bottom: '0%',
-    },
-    series: [
-        {
-            name: '博客类型',
-            type: 'pie',
-            radius: '50%',
-            data: [
-                { value: 1, name: '性能优化' },
-                { value: 2, name: 'Vue' },
-                { value: 2, name: '杂谈' },
-            ],
-            emphasis: {
-                itemStyle: {
-                    shadowBlur: 10,
-                    shadowOffsetX: 0,
-                    shadowColor: 'rgba(0, 0, 0, 0.5)',
-                },
-            },
-        },
-    ],
-});
 </script>
 
 <template>
     <div class="p-6 h-[89vh] overflow-y-auto">
-        <el-card class="mb-6">
-            <h2 class="text-lg font-semibold mb-4">博客类型统计</h2>
-            <VueECharts :options="blogTypeChartOptions" class="w-full h-96"></VueECharts>
-        </el-card>
-
         <div class="grid grid-cols-2 gap-6">
             <el-card>
                 <h2 class="text-lg font-semibold mb-4">最近博客</h2>
                 <el-row :gutter="20">
                     <el-col :span="6" v-for="blog in recentBlog" :key="blog.id">
-                        <el-card shadow="hover">
+                        <el-card shadow="hover" @click="router.push(`/blogedit?id=${blog.id}`)">
                             <h3 class="font-semibold text-base mb-1">{{ blog.title.slice(0, 15) }}...</h3>
                             <p class="text-gray-500 text-sm">状态: {{ blog.status === 'published' ? "已发布" : "编辑中" }}</p>
                         </el-card>
@@ -79,8 +46,9 @@ const blogTypeChartOptions = ref({
                 <h2 class="text-lg font-semibold mb-4">最近作品</h2>
                 <el-row :gutter="20">
                     <el-col :span="6" v-for="work in recentWork" :key="work.id">
-                        <el-card shadow="hover">
-                            <a :href="work.link" target="_blank" class="text-global font-semibold">{{ work.name }}</a>
+                        <el-card shadow="hover" @click="router.push(`/workadd?id=${work.id}`)">
+                            <a :href="work.link" target="_blank" class="text-global font-semibold mb-1">{{ work.name
+                                }}</a>
                             <p class="text-gray-500 text-sm mt-1">版本: {{ work.vision }}</p>
                         </el-card>
                     </el-col>
